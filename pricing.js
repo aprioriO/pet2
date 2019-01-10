5ab5d0b0fb76e1fefdad87777b56c7e32a43efb8
Olena Hulakova <ohulakova@gmail.com>
2019-01-10 22:13:50 +0000

    Minimalizm

--- pricing.js
+++ pricing.js
@@ -1,10 +1,6 @@
 $(() => {
   const price = 299
-  function addTotal() {
-    var newTotal = ($(".js-quantity").val()) * price
-    console.log(newTotal);
-    $(".js-total").text(`$${newTotal}`)
-    console.log("new total is:" + newTotal);
-  }
-  $(".js-button").on("click", addTotal)
+  $(".js-button").on("click", function() {
+    $(".js-total").text('$' + $(".js-quantity").val() * price)
+  })
 })
