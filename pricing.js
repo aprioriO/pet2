52773fa21d2bdf47b970b0c0a206ab6544517943
Olena Hulakova <ohulakova@gmail.com>
2019-01-10 22:12:14 +0000

    Simplify

--- pricing.js
+++ pricing.js
@@ -1,11 +1,9 @@
 $(() => {
   const price = 299
   function addTotal() {
-    $(".js-total-para").find(".js-total").remove()
-    console.log("remove span");
     var newTotal = ($(".js-quantity").val()) * price
     console.log(newTotal);
-    $(".js-total-para").append(`<span class="js-total"> $${newTotal} </span>`)
+    $(".js-total").text(`$${newTotal}`)
     console.log("new total is:" + newTotal);
   }
   $(".js-button").on("click", addTotal)
