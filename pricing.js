3f011c530f7de2bc23edf22a0c83ed3b5e2cf6d7
Olena Hulakova <ohulakova@gmail.com>
2019-01-10 22:21:21 +0000

    Compare two ways of coding

--- pricing.js
+++ pricing.js
@@ -4,3 +4,23 @@ $(() => {
     $(".js-total").text('$' + $(".js-quantity").val() * price)
   })
 })
+
+// $(() => {
+//   const price = 299
+//   const $total = $(".js-total")
+//   const $quantity = $(".js-quantity")
+//
+//   function getQuantity() {
+//     return $quantity.val()
+//   }
+//
+//   function calculateTotalPrice() {
+//     return getQuantity() * price
+//   }
+//
+//   function showTotal() {
+//     $total.text('$' + calculateTotalPrice())
+//   }
+//
+//   $(".js-button").on("click", showTotal)
+// })
