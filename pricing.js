46de5498ba4d50cc7491ebd7c9b2c102fcbd4015
Olena Hulakova <ohulakova@gmail.com>
2019-01-10 22:07:43 +0000

    Reformat and remove trash

--- pricing.js
+++ pricing.js
@@ -1,16 +1,12 @@
-console.log('sdfsd');
-$(document).ready(function () {
-//299
+$(() => {
   const price = 299
-   function addTotal() {
+  function addTotal() {
     $(".js-total-para").find(".js-total").remove()
     console.log("remove span");
     var newTotal = ($(".js-quantity").val()) * price
     console.log(newTotal);
     $(".js-total-para").append(`<span class="js-total"> $${newTotal} </span>`)
     console.log("new total is:" + newTotal);
-   }
-  $(".js-button").on("click", function(){
-    addTotal()
-  })
+  }
+  $(".js-button").on("click", addTotal)
 })
