8e4b4923bfc9c170643cc65e71b770d0a98ca082
Olena Hulakova <ohulakova@gmail.com>
2019-01-11 18:30:12 +0000

    Draft without jQuery

---
+++ pricing2.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function() {
+  console.log('Your document is ready!');
+
+  const price = 299;
+  var  elementCollection = document.getElementsByClassName('js-total')
+  console.log (elementCollection);
+
+  var totalElement = elementCollection.item(0) // find span element with initial total value
+  console.log(totalElement);
+
+  // var inputElement = document.getElementById('quantity')
+  // var inputValue = inputElement.value
+
+  var inputElementCollection = document.getElementsByClassName('js-quantity')
+  var inputElement = inputElementCollection.item(0)
+  var inputValue = inputElement.value
+  console.log(`inputValue: ${inputValue}`);
+
+  var newTotal = inputValue * price
+  console.log(newTotal);
+
+});
+
+
+
+// $(() => {
+//   const price = 299
+//
+//   function showTotal() {
+//     $(".js-total").text('$' + $(".js-quantity").val() * price)
+//   }
+//   $(".js-button").on("click", showTotal)
+//   $('.js-quantity').on("change keyup", showTotal)
+// })
+
+// textContent
+// console.log(initTotal);
+// var inputValue = document.getElementsByClassName('js-quantity').value;
+// var total = inputValue * price;
+// var createTextInTotal = document.createTextNode(total)
+// function showTotal(){
+//   initTotal.appendChild(createTextInTotal)
+// }
+//
+// showTotal();
