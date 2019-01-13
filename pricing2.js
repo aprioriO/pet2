5257d4fca9b39eeaf6a5dc2153e26a4f64df2913
Olena Hulakova <ohulakova@gmail.com>
2019-01-13 15:50:11 +0000

    DRY quantityEl

--- pricing2.js
+++ pricing2.js
@@ -2,8 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
   const price = 299;
 
-  function showTotal(card) {
-    var quantityEl = card.querySelector('.js-quantity')
+  function showTotal(card, quantityEl) {
     var quantity = quantityEl.value
     var totalEl = card.querySelector('.js-total')
     totalEl.innerText = "$" + quantity * price
@@ -11,15 +10,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
   var allCards  = document.querySelectorAll('.card')
   allCards.forEach(function(card) {
+    // var card
+    var quantityEl = card.querySelector('.js-quantity')
 
     function eventHandler() {
-      showTotal(card) // card = card
+      showTotal(card, quantityEl) // card = card
     }
 
     var calculateBtn = card.querySelector('.js-button')
     calculateBtn.addEventListener('click', eventHandler)
 
-    var quantityEl = card.querySelector('.js-quantity')
     quantityEl.addEventListener('keyup', eventHandler)
     quantityEl.addEventListener('change', eventHandler)
   })
