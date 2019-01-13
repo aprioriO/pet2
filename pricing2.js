c360568c99fbf532883acdca0f43dd7768d8fb5c
Olena Hulakova <ohulakova@gmail.com>
2019-01-13 16:36:20 +0000

    Style it up with standart

--- pricing2.js
+++ pricing2.js
@@ -1,19 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function () {
+  const price = 299
 
-  const price = 299;
-
-  function calcAndShowPrice({card, quantityEl, totalEl}) { // q->t
+  function calcAndShowPrice ({ card, quantityEl, totalEl }) { // q->t
     var quantity = quantityEl.value
-    totalEl.innerText = "$" + quantity * price
+    totalEl.innerText = '$' + quantity * price
   }
 
   var allCards = document.querySelectorAll('.card')
-  allCards.forEach(function(card) {
+  allCards.forEach(function (card) {
     var totalEl = card.querySelector('.js-total')
     var quantityEl = card.querySelector('.js-quantity')
 
-    function eventHandler() {
-      calcAndShowPrice({card, totalEl, quantityEl}) // t->q
+    function eventHandler () {
+      calcAndShowPrice({ card, totalEl, quantityEl }) // t->q
     }
 
     var calculateBtn = card.querySelector('.js-button')
