5dce8c1481b865c5605a3faa4d03fa9144f79c24
Olena Hulakova <ohulakova@gmail.com>
2019-01-13 15:23:08 +0000

    Make it to calculate total price

--- pricing2.js
+++ pricing2.js
@@ -2,27 +2,38 @@ document.addEventListener("DOMContentLoaded", function() {
   console.log('Your document is ready!');
 
   const price = 299;
-  var totalElement = document.querySelector('.js-total') // find span element with initial total value
-  var inputElement = document.querySelector('.js-quantity')
+  // var totalElement = document.querySelector('.js-total') // find span element with initial total value
+  // var inputElement = document.querySelector('.js-quantity')
 
-  inputElement.addEventListener("keyup", showTotal)
-  inputElement.addEventListener("change", showTotal)
+  // inputElement.addEventListener("keyup", showTotal)
+  // inputElement.addEventListener("change", showTotal)
+  //
+  // var myButton = document.querySelector('.js-button')
+  // myButton.addEventListener("click", showTotal)
 
-  var myButton = document.querySelector('.js-button')
-  myButton.addEventListener("click", showTotal)
-
-  function showTotal() {
-    totalElement.innerHTML = "$" + inputElement.value * price
-  }
+  // function showTotal() {
+  //   totalElement.innerHTML = "$" + inputElement.value * price
+  // }
 
   // -- try code
 
+  function showTotal(card) {
+    var totalElement = card.querySelector('.js-total')
+    var quantityEl = card.querySelector('.js-quantity')
+    var quantity = quantityEl.value
+    totalElement.innerText = "$" + quantity * price
+  }
+
+
   var allCards  = document.querySelectorAll('.card')
   allCards.forEach(function(card) {
-    console.log(card);
     var calculateBtn = card.querySelector('.js-button')
-    console.log(calculateBtn);
+    // var card
+
+    function eventHandler() {
+      showTotal(card) // card = card
+    }
+
+    calculateBtn.addEventListener("click", eventHandler)
   })
-
-
 })
