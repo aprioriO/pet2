c7d3b361bf438eabcfc24bd23f903813043c337a
Olena Hulakova <ohulakova@gmail.com>
2019-01-13 12:19:35 +0000

    Use document.querySelector() method

--- pricing2.js
+++ pricing2.js
@@ -19,23 +19,21 @@
 //   showTotal()
 // });
 
+
 document.addEventListener("DOMContentLoaded", function() {
   console.log('Your document is ready!');
 
   const price = 299;
-  var totalElement = document.getElementsByClassName('js-total').item(0) // find span element with initial total value
-  var inputElement = document.getElementsByClassName('js-quantity').item(0)
+  var totalElement = document.querySelector('js-total')// find span element with initial total value
+  var inputElement = document.querySelector('js-quantity')
 
   inputElement.addEventListener("keyup", showTotal)
   inputElement.addEventListener("change", showTotal)
 
-  var myButton = document.getElementById('js-btn')
+  var myButton = document.querySelector('js-btn')
   myButton.addEventListener("click", showTotal)
 
-
   function showTotal() {
-    var inputValue = inputElement.value
-    var newTotal = inputValue * price
-    totalElement.innerHTML = "$" + newTotal
+    totalElement.innerHTML = "$" + inputElement.value * price
   }
 })
